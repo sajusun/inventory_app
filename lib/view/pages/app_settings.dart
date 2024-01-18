958232d553e7afbaf1e69f489fd4161e35c06226
sajusun <sajuislam266@gmail.com>
2024-01-18 20:44:41 +0600

    edit text style abd app drawer

---
+++ lib/view/pages/app_settings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppSettings extends StatelessWidget {
+  const AppSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(
+      child: const Center(child: Text("setting page")),
+    ),);
+  }
+}
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -8,137 +8,156 @@ import 'package:inventoryapp/view/pages/item_model.dart';
 import 'package:inventoryapp/view/pages/items.dart';
 import 'package:inventoryapp/view/widgets/text_style.dart';
 import '../../presenters/controller/getx_controller.dart';
+import 'app_settings.dart';
 
+@immutable
 class HomePage extends StatefulWidget {
-  String title;
+  final String title;
 
-  HomePage({super.key, required this.title});
+  const HomePage({super.key, required this.title});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-    var controller = Get.put(ValController());
+  var controller = Get.put(ValController());
+
   @override
   Widget build(BuildContext context) {
     HomePageCtrl.getAllItems();
     return Scaffold(
 
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        clipBehavior: Clip.hardEdge,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView(
-            children: [
-               SizedBox(
-                height: 100,
-                child: Column(
-                  children: [
-                   Image.network("https://w7.pngwing.com/pngs/487/46/png-transparent-logo-black-and-white-brand-letter-d-white-text-rectangle.png",height: 60,),
-                    const Text("Company Title",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
-                  ],
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        drawer: Drawer(
+          clipBehavior: Clip.hardEdge,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListView(
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: Column(
+                    children: [
+                      Image.network(
+                        "https://w7.pngwing.com/pngs/487/46/png-transparent-logo-black-and-white-brand-letter-d-white-text-rectangle.png",
+                        height: 60,),
+                      const Text("Company Title", style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20),)
+                    ],
+                  ),
                 ),
-              ),
-Container(
-  padding: EdgeInsets.only(left: 30),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      TextButton.icon(onPressed: () {
-        Get.to(()=>const Category());
-      }, icon: const Icon(Icons.arrow_right),
-      label:  Text("Product Category",style: textStyle0,)),
+                Container(
+                  padding: const EdgeInsets.only(left: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextButton.icon(onPressed: () {
+                        Get.to(() => const Category());
+                      }, icon: const Icon(Icons.arrow_right),
+                          label: Text("Product Category", style: textStyle0,)),
 
-      TextButton.icon(onPressed: () {
-        Get.to(()=> const ProductModel());
-      }, icon: const Icon(Icons.arrow_right),
-      label:  Text("Product Model",style: textStyle0,)),
+                      TextButton.icon(onPressed: () {
+                        Get.to(() => const ProductModel());
+                      }, icon: const Icon(Icons.arrow_right),
+                          label: Text("Product Model", style: textStyle0,)),
 
-      TextButton.icon(onPressed: () {
-        Get.to(()=> const Items());
-      }, icon: const Icon(Icons.arrow_right),
-      label:  Text("Add Items",style: textStyle0,)),
-      TextButton.icon(onPressed: () {
-        Get.to(()=>const AddProduct());
-      }, icon: const Icon(Icons.arrow_right),
-          label:  Text("Add Products",style: textStyle0,)),
-    ],
-  ),
-)
-            ],
+                      TextButton.icon(onPressed: () {
+                        Get.to(() => const Items());
+                      }, icon: const Icon(Icons.arrow_right),
+                          label: Text("Add Items", style: textStyle0,)),
+                      TextButton.icon(onPressed: () {
+                        Get.to(() => const AddProduct());
+                      }, icon: const Icon(Icons.arrow_right),
+                          label: Text("Add Products", style: textStyle0,)),
+                    ],
+                  ),
+                ),
+
+                IconButton(
+                  alignment: Alignment.bottomRight,
+                  onPressed: (){
+                  Get.to(() => const AppSettings());
+                }, icon: const Icon(Icons.settings,),)
+              ],
+            ),
           ),
         ),
-      ),
-      body: SizedBox(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-         child: allProductList(),
-        ),
-      )
+        body: SizedBox(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: allProductList(),
+          ),
+        )
     );
-  }   //   build context
+  } //   build context
 
 //   all product list function
-Widget allProductList(){
-   return Obx(() {
-     if (controller.allProductList.isNotEmpty) {
-       return ListView.builder(
-           itemCount: controller.allProductList.length,
-           itemBuilder: (context, index){
-         return Padding(
-           padding: const EdgeInsets.only(top: 8.0),
-           child: Container(
-             decoration: BoxDecoration(
-               color: Colors.cyan,
-               borderRadius: BorderRadius.circular(8),
-               border: Border.all()
-             ),
-             child: Column(
-               children: [
-                 Text(" ${controller.allProductList[index].itemName}  - " " ${controller.allProductList[index].modelName}"),
-                 Text( " Category : ${controller.allProductList[index].category}, " " Available Items : ${controller.allProductList[index].quantity} "),
-               ],
-             ),
-           ),
-         );
-       });
-     } else {
-       return  const Center(child: CircularProgressIndicator());
-     }
-   });
+  Widget allProductList() {
+    return Obx(() {
+      if (controller.allProductList.isNotEmpty) {
+        return ListView.builder(
+            itemCount: controller.allProductList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all()
+                  ),
+                  child: Column(
+                    children: [
+                      Text(" ${controller.allProductList[index]
+                          .itemName}  - " " ${controller.allProductList[index]
+                          .modelName}"),
+                      Text(" Category : ${controller.allProductList[index]
+                          .category}, " " Available Items : ${controller
+                          .allProductList[index].quantity} "),
 
-}
+                    ],
+                  ),
+                ),
+              );
+            });
+      } else {
+        return const Center(child: CircularProgressIndicator());
+      }
+    });
+  }
 
-Widget futureView(){
-  return  FutureBuilder(future:ProductsModel().getAllItems(),
-        builder: (context, AsyncSnapshot<List<Products>> data){
-      return ListView.builder(
-          itemCount: data.data?.length,
-          itemBuilder: (context, index){
-            return Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all()
-                ),
-                child: Column(
-                  children: [
-                    Text(" ${data.data?[index].itemName}  - " " ${data.data?[index].modelName}"),
-                    Text( " Category : ${data.data?[index].category}, " " Available Items : ${data.data?[index].quantity} "),
-                  ],
-                ),
-              ),
-            );
-      });
+  Widget futureView() {
+    return FutureBuilder(future: ProductsModel().getAllItems(),
+        builder: (context, AsyncSnapshot<List<Products>> data) {
+          return ListView.builder(
+              itemCount: data.data?.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.cyan,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all()
+                    ),
+                    child: Column(
+                      children: [
+                        Text(" ${data.data?[index].itemName}  - " " ${data
+                            .data?[index].modelName}"),
+                        Text(" Category : ${data.data?[index]
+                            .category}, " " Available Items : ${data
+                            .data?[index].quantity} "),
+                      ],
+                    ),
+                  ),
+                );
+              });
         });
-}
+  }
 
 
 }
