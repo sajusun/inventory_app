896a91a53fc6abc56e07bab89cb427d6857b1d7f
sajusun <sajuislam266@gmail.com>
2024-01-31 00:07:47 +0600

    localDB changes

--- lib/model/local_db/category_model.dart
+++ lib/model/local_db/category_model.dart
@@ -2,13 +2,13 @@ import 'package:inventoryapp/model/local_db/local_db.dart';
 import 'package:sqflite/sqflite.dart';
 
 class CategoryModel {
-  late Database database;
+  //late Database database;
   MyDb db = MyDb();
 
   // add category
   Future<bool> add(String name) async {
-    database = await db.initDB();
-    int result = await database
+    //database = await db.initDB();
+    int result = await db.database
         .rawInsert("INSERT INTO ${db.category} (name) VALUES (?);", [name]);
 
     if (result > 0) {
@@ -21,13 +21,24 @@ class CategoryModel {
   //retrieve all category
   Future<List<Map<String, dynamic>>> fetch() async {
     await db.initDB();
-    return await database.query(db.category);
+    return await db.database.query(db.category);
   }
 
   Future<bool> delete(int id) async {
     await db.initDB();
     int result =
-        await database.delete(db.category, where: 'id = ?', whereArgs: [id]);
+        await db.database.delete(db.category, where: 'id = ?', whereArgs: [id]);
+    if (result > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> rename(String name, int id) async {
+    await db.initDB();
+    int result = await db.database.update(db.category, {"name": name},
+        where: 'id = ?', whereArgs: [id]);
     if (result > 0) {
       return true;
     } else {
