8f7a456a4833e7c2ebcaf4d0fb493f9328717318
sajusun <sajuislam266@gmail.com>
2024-03-20 19:49:52 +0600

    login deesign

--- lib/view/pages/login.dart
+++ lib/view/pages/login.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-   Login({super.key});
+  Login({super.key});
 
-  TextEditingController email =TextEditingController();
-   TextEditingController pass =TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("userLogin"),centerTitle: true,),
+      appBar: AppBar(title: Text("userLogin"), centerTitle: true,),
       body: Container(
         child: Column(
           children: [
-            TextFormField(controller: email,),
-            TextFormField(controller: pass,),
+          TextFormField(controller: email,
+            style: const TextStyle(height: 1),
+            decoration: const InputDecoration(border: UnderlineInputBorder(),labelText: "Password"),
+          ),
+        TextFormField(controller: pass,
+          style: const TextStyle(height: 1),
+          decoration: const InputDecoration(border: UnderlineInputBorder(),labelText: "Password"),
+        ),
+        SizedBox(height: 20,),
+        ElevatedButton(onPressed: () {}, child: const Text("Login")
+        ),
           ],
         ),
       ),
